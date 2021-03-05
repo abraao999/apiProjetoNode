494a1879f4c8e2b49c0ac202eee3392a5c7da524
abraao <abraaocpm@hotmail.com>
2021-03-04 22:32:50 -0300

    add cors config acess e helmet

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,23 @@ _dotenv2.default.config();
 require('./database');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
 var _tokemRoutes = require('./routes/tokemRoutes'); var _tokemRoutes2 = _interopRequireDefault(_tokemRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
+
+const whiteList = [
+  // dados do servidor
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) callback(null, true);
+    else callback(new Error('not allowed by cors'));
+  },
+};
 
 class App {
   constructor() {
@@ -21,7 +33,9 @@ class App {
   middlewares() {
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
+    // this.app.use(cors(corsOptions));
     this.app.use(_cors2.default.call(void 0, ));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
   }
 
--- src/app.js
+++ src/app.js
@@ -5,11 +5,23 @@ dontenv.config();
 import './database';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokemRoutes from './routes/tokemRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  // dados do servidor
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) callback(null, true);
+    else callback(new Error('not allowed by cors'));
+  },
+};
 
 class App {
   constructor() {
@@ -21,7 +33,9 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    // this.app.use(cors(corsOptions));
     this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
